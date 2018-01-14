913f4434825541605c4e91928d445b7bcbe024fd
billodell <billodell5@gmail.com>
2018-01-14 15:31:13 -0800

    reworked start function

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
-var querystring = require("querystring");
+var querystring = require('querystring'),
+  fs = require('fs');
 
 function start(response, postData) {
   console.log('Request handler \'start\' was called.');
@@ -9,9 +10,10 @@ function start(response, postData) {
  'charset=UTF-8" />' +
  '</head>' +
  '<body>' +
- '<form action="/upload" method="post">' +
- '<textarea name="text" rows="20" cols="60"></textarea>' +
- '<input type="submit" value="Submit text" />' +
+ '<form action="/upload" enctype="multipart/form-data" ' +
+ 'method="post">' +
+ '<input type="file" name="upload">' +
+ '<input type="submit" value="Upload file" />' +
  '</form>' +
  '</body>' +
  '</html>';
@@ -23,9 +25,14 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log('Request handler \'upload\' was called.');
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write("You\'ve sent the text: " + querystring.parse(postData).text);
+  response.write('You\'ve sent the text: ' + querystring.parse(postData).text);
   response.end();
 }
-
+function show(response) {
+  console.log('Request handler \'show\' was called.');
+  response.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(response);
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
