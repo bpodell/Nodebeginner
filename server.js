0153a41dcdeb839d5d0b60f58f58f18a6f4e79e5
billodell <billodell5@gmail.com>
2018-01-14 15:11:16 -0800

    updated responses

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ var url = require("url");
 //   response.write("Hello World");
 //   response.end();
 // }).listen(8888);
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(pathname);
+    
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
